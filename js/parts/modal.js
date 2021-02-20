9acad55529617d2112ff7003752c1ae43a258f30
Madiyar Esmuhambetov <72384095+Madiyarsite@users.noreply.github.com>
2021-02-20 12:46:01 +0600

    Add files via upload

--- js/parts/modal.js
+++ js/parts/modal.js
@@ -2,8 +2,6 @@ function modal(){
     let menu = document.querySelector('.menu'),
         hbtn = document.querySelector('.header__btn'),
         breaker = document.querySelector('.modal__item'),
-        main = document.getElementById('main'),
-        about = document.getElementById('about'),
         section = document.querySelectorAll('.section'),
         btnmenu = document.querySelectorAll('.menu__item'),
         footer = document.querySelector('.footer'),
@@ -13,23 +11,20 @@ function modal(){
     });
     close.addEventListener('click', function(){
         menu.classList.remove('active');
-        
     });
-    
     function back(){
         breaker.style.display = 'none';
     }
     function foot(i){
-         
         if(i > 0){
             footer.style.display = 'flex';
+        }else{
+            footer.style.display = 'none';
         }
     }
     btnmenu.forEach((item, i)=> item.addEventListener('click', function(){
             breaker.style.display = "block";
             section.forEach((item)=> item.style.display = 'none');
-            
-             
             setTimeout(block, 1100);
             setTimeout(next, 750);
             setTimeout(back, 3000);
@@ -37,19 +32,9 @@ function modal(){
                 section[i].style.display = 'block';
             }
             function next(){
-                menu.classList.remove('active');
-                
-                
-            }
-           
-            
+                menu.classList.remove('active'); 
+            }  
             foot(i);
-      
-    }));
-   
-   
-    
-        
-    
+    })); 
 }
 module.exports = modal;
