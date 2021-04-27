aa8cb4e3f905bcb18b5dcd27786a8a33087f06ce
Madiyar Esmuhambetov <72384095+Madiyarsite@users.noreply.github.com>
2021-04-27 19:56:24 +0600

    Add files via upload

--- js/bundle.js
+++ js/bundle.js
@@ -146,13 +146,18 @@ module.exports = modal;
 /***/ ((module) => {
 
 function percent(){
+    let lod = document.querySelector('.loaded');
+    function del(){
+        lod.remove();
+    }
+    del();
     let percentItem = document.querySelectorAll('.about__progress_item'),
         percentWrapp = document.querySelector('.about__skills');
         
         window.addEventListener('scroll', function(){
-           let perTop = percentWrapp.getBoundingClientRect().top + 30,
+           let perTop = percentWrapp.getBoundingClientRect().top ,
             realTop = window.pageYOffset; 
-            if(perTop < realTop){
+            if(perTop + 100 < realTop){
                 percentItem.forEach((item)=> item.classList.remove('percent'));
                 done();
             }
@@ -160,39 +165,42 @@ function percent(){
             
         });
         function done(){
-            start = setInterval(strikePlus,  98);
+            start = setInterval(strikePlus,  60);
         }
-        let html = document.querySelector('.html'),
+        
+            
+            let html = document.querySelector('.html'),
             js = document.querySelector('.js'),
             vue = document.querySelector('.vue');
-        
-            let strike = 0;
+           
+            strike = 0;
                 
-            
             function strikePlus(){
+               
+               strike += 0.1;
                 if(strike < 95){
-                    
+                    console.log(strike);
                     html.innerHTML = Math.round(strike) + '%' ;
-                   
+                   html.style.width = strike + "%";
                 }if(strike < 70){
                     
                    
                     js.innerHTML = Math.round(strike) + '%'  ;
-                    
+                    js.style.width = strike + "%";
                 }if(strike < 85){
                    
                     
                     vue.innerHTML = Math.round(strike) + '%'  ;
-                    
+                    vue.style.width = strike + "%";
                     
                 }if(strike == 98){
                     clearInterval(start);
                 }else{
                     
-                    console.log(strike);
+                    
                 }
-                console.log(percentItem[0].clientWidth);
-               strike += 0.1;
+                
+               
             
         }
     
